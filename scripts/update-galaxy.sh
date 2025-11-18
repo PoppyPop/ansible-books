8bd38495e79ce2cd70185afc35a627021c3e6aa3
PoppyPop <skytep@gmail.com>
2025-11-18 21:01:53 +0000

    Update galaxy

---
+++ scripts/update-galaxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+REQ_FILE="requirements.yml"
+
+if [[ ! -f "$REQ_FILE" ]]; then
+    echo "requirements.yml not found!"
+    exit 1
+fi
+
+echo "Updating Ansible Galaxy roles and collections..."
+ansible-galaxy install -r "$REQ_FILE" --force
+ansible-galaxy collection install -r "$REQ_FILE" --force
+
+echo "Done!"
